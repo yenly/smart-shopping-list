36b89997331a71f0a6beae759cb2ef2cbea91a73
Yenly <yencodes@gmail.com>
2021-01-06 12:06:22 -0800

    Use react-router-dom to add app routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import MessageFirebase from './components/MessageFirebase';
+import ListItems from './components/ListItems';
+import AddItem from './components/AddItem';
+import Navigation from './components/Navigation';
 
 function App() {
-  return <MessageFirebase />;
+  return (
+    <>
+      <header>Smart Shopping List App</header>
+      <Router>
+        <main>
+          <Switch>
+            <Route path="/list">
+              <ListItems />
+            </Route>
+            <Route path="/add">
+              <AddItem />
+            </Route>
+            <Route exact path="/">
+              <MessageFirebase />
+            </Route>
+          </Switch>
+        </main>
+        <Navigation />
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/AddItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddItem = () => {
+  return <div>Add item view</div>;
+};
+
+export default AddItem;
