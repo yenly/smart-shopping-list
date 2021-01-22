32ab5cce87247e56d0c80a2fff053519e70d2492
Yenly <yencodes@gmail.com>
2021-01-22 15:22:48 -0800

    Allow user to enter existing token to join a list

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ import Navigation from './components/Navigation';
 import Home from './components/Home';
 import getToken from './lib/tokens';
 import { FirebaseContext } from './components/Firebase';
+// import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 function App() {
   const [userToken, setUserToken] = useState('');
+  const [alertMsg, setAlertMsg] = useState('');
   const firebase = useContext(FirebaseContext);
   const db = firebase.firestore();
 
@@ -49,6 +51,26 @@ function App() {
     }
   };
 
+  const submitListToken = (event) => {
+    event.preventDefault();
+    const listToken = event.target.listToken.value;
+    const list = db.collection(listToken);
+    list.get().then((doc) => {
+      if (!doc.empty) {
+        setUserToken(listToken.trim());
+        window.localStorage.setItem('shoppingListAppUser', listToken);
+      } else {
+        setAlertMsg({
+          message: 'Token does not exist. Please try again or create new list.',
+          msgType: 'danger',
+        });
+        setTimeout(() => {
+          setAlertMsg('');
+        }, 3000);
+      }
+    });
+  };
+
   return (
     <ThemeProvider theme={sketchy}>
       <header>Smart Shopping List</header>
@@ -73,7 +95,11 @@ function App() {
               {userToken ? (
                 <Redirect to="/list" />
               ) : (
-                <Home createUserToken={createUserToken} />
+                <Home
+                  createUserToken={createUserToken}
+                  submitListToken={submitListToken}
+                  alertMsg={alertMsg}
+                />
               )}
             </Route>
           </Switch>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import { Button } from 'theme-ui';
+import React, { useState } from 'react';
+import { Button, Label, Input, Box } from 'theme-ui';
 import PropTypes from 'prop-types';
+import Notification from './Notification';
 
-const Home = ({ createUserToken }) => {
+const Home = ({ createUserToken, submitListToken, alertMsg }) => {
+  const [listToken, setListToken] = useState('');
+
+  const onChangeListToken = (event) => {
+    setListToken(event.target.value);
+  };
+
   return (
     <div>
       <h1>Welcome!</h1>
       <Button onClick={createUserToken}>Create new list</Button>
+
+      <h3>OR</h3>
+      <p>Join an existing shopping list by entering a three word token</p>
+
+      {alertMsg && (
+        <Notification message={alertMsg.message} msgType={alertMsg.msgType} />
+      )}
+
+      <Box as="form" onSubmit={submitListToken}>
+        <Label htmlFor="listToken">Share token</Label>
+        <Input
+          mb={3}
+          id="listToken"
+          value={listToken}
+          onChange={onChangeListToken}
+        />
+        <Button type="submit">Join an existing item</Button>
+      </Box>
     </div>
   );
 };
 
 Home.propTypes = {
   createUserToken: PropTypes.func.isRequired,
+  submitListToken: PropTypes.func.isRequired,
 };
 
 export default Home;
