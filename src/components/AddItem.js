ae7116852b3222ced489e9b3bccd44ec89c8b252
Yenly <yencodes@gmail.com>
2021-01-23 00:51:07 -0800

    Check for duplicate in db before adding item

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,10 +1,10 @@
 import { Fragment } from 'react';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 /** @jsx jsx */
 import { jsx, Box, Label, Input, Radio, Button } from 'theme-ui';
-import Notification from './Notification';
+import { FirebaseContext } from './Firebase';
 
 const RadioOptions = {
   soon: 7,
@@ -12,10 +12,11 @@ const RadioOptions = {
   'not soon': 30,
 };
 
-const AddItem = ({ addItem }) => {
+const AddItem = ({ userToken, setAlertMsg }) => {
   const [itemName, setItemName] = useState('');
   const [likelyToPurchase, setLikelyToPurchange] = useState(RadioOptions.soon);
-  const [alertMsg, setAlertMsg] = useState('');
+  const firebase = useContext(FirebaseContext);
+  const db = firebase.firestore();
   let history = useHistory();
 
   const onChangeItemName = (event) => {
@@ -26,34 +27,50 @@ const AddItem = ({ addItem }) => {
     setLikelyToPurchange(Number(event.target.value));
   };
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-    if (!event.target.itemName.value) {
-      setAlertMsg({
-        message: 'Please enter item name',
-        msgType: 'danger',
-      });
-      setTimeout(() => {
-        setAlertMsg('');
-      }, 3000);
-    } else {
-      addItem(event);
-      setItemName('');
-      setLikelyToPurchange(7);
-      setAlertMsg({
-        message: 'Item added!',
-        msgType: 'info',
-      });
-      setTimeout(() => {
-        history.push('/list');
-      }, 3000);
+    if (event.target.itemName.value) {
+      const itemName = event.target.itemName.value;
+      const likelyToPurchase = Number(event.target.likelyToPurchase.value);
+      const newItem = {
+        name: itemName.trim(),
+        likelyToPurchase: likelyToPurchase,
+        purchaseDate: null,
+      };
+      if (userToken) {
+        const itemRef = db.collection(userToken);
+        const snapshot = await itemRef.where('name', '==', itemName).get();
+
+        if (!snapshot.empty) {
+          setAlertMsg({
+            message: `${itemName} is already on the list.`,
+            msgType: 'danger',
+          });
+          setItemName('');
+          setTimeout(() => {
+            setAlertMsg('');
+          }, 3000);
+        } else {
+          db.collection(userToken)
+            .add(newItem)
+            .then((docRef) => console.log(`New item added: ${docRef.id}`))
+            .catch((error) => console.error(`Error adding item: ${error}`));
+          setAlertMsg({
+            message: 'Item added!',
+            msgType: 'info',
+          });
+          setItemName('');
+          setLikelyToPurchange(7);
+          setTimeout(() => {
+            setAlertMsg('');
+            history.push('/list');
+          }, 2000);
+        }
+      }
     }
   };
   return (
     <div>
-      {alertMsg && (
-        <Notification message={alertMsg.message} msgType={alertMsg.msgType} />
-      )}
       <Box as="form" onSubmit={onSubmit}>
         <Label htmlFor="itemName">Item Name</Label>
         <Input
@@ -61,6 +78,7 @@ const AddItem = ({ addItem }) => {
           id="itemName"
           value={itemName}
           onChange={onChangeItemName}
+          required
         />
         <Label htmlFor="likelyToPurchase">
           How soon are you likely to buy again?
@@ -88,7 +106,8 @@ const AddItem = ({ addItem }) => {
 };
 
 AddItem.propTypes = {
-  addItem: PropTypes.func.isRequired,
+  userToken: PropTypes.string.isRequired,
+  setAlertMsg: PropTypes.func.isRequired,
 };
 
 export default AddItem;
